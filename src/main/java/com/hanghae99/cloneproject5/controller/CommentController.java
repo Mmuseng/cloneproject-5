49534186dc352b0a2ac6cecd7c499648319737ab
MinjuKim <kpoong02@naver.com>
2022-06-19 02:23:53 +0900

    comment service / controller add

---
+++ src/main/java/com/hanghae99/cloneproject5/controller/CommentController.java
@@ -0,0 +1,44 @@
+package com.hanghae99.cloneproject5.controller;
+
+import com.hanghae99.cloneproject5.dto.commentDto.CommentRequestDto;
+import com.hanghae99.cloneproject5.dto.commentDto.CommentResponseDto;
+import com.hanghae99.cloneproject5.dto.commentDto.CreateCommentResponseDto;
+import com.hanghae99.cloneproject5.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class CommentController {
+
+    private final CommentService commentService;
+
+//    @PostMapping("/comments")
+//    public CreateCommentResponseDto createComment(@PathVariable Long boardId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+//        return commentService.createComment(boardId, requestDto, userDetails);
+//    }
+
+    // 댓글 조회
+    @GetMapping("/comments/{boardId}")
+    public List<CommentResponseDto> getComments(@PathVariable Long boardId) {
+
+        return commentService.getComment(boardId);
+    }
+
+    // 댓글 수정
+    @PutMapping("/comments/update/{commentid}")
+    public void updateComment(@PathVariable Long commentId, @RequestBody CommentRequestDto requestDto) {
+
+        commentService.updateComment(commentId, requestDto);
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/comments/{commentid}")
+    public void deleteComment(@PathVariable Long commentId){
+
+        commentService.deleteComment(commentId);
+    }
+}
--- src/main/java/com/hanghae99/cloneproject5/service/CommentService.java
+++ src/main/java/com/hanghae99/cloneproject5/service/CommentService.java
@@ -1,8 +1,11 @@
 package com.hanghae99.cloneproject5.service;
 
 import com.hanghae99.cloneproject5.dto.commentDto.CommentRequestDto;
+import com.hanghae99.cloneproject5.dto.commentDto.CommentResponseDto;
+import com.hanghae99.cloneproject5.dto.commentDto.CreateCommentResponseDto;
 import com.hanghae99.cloneproject5.model.Board;
 import com.hanghae99.cloneproject5.model.Comment;
+import com.hanghae99.cloneproject5.model.Member;
 import com.hanghae99.cloneproject5.repository.BoardRepository;
 import com.hanghae99.cloneproject5.repository.CommentRepository;
 import com.hanghae99.cloneproject5.repository.MemberRepository;
@@ -11,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -21,8 +26,20 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final MemberRepository memberRepository;
 
-    //public CommentRegisterResponseDto registerComment(Long boardId, CommentRegisterDto requestDto, UserDetailsImpl userDetails) {
-    //Board board = boardRepository.findbyId(boardId).orElseThrow(() -> new NullPointerException("registerComment ID를 찾을 수 없습니다."));
+//    public CommentRegisterResponseDto createComment(Long boardId, CommentRegisterDto requestDto, UserDetailsImpl userDetails) {
+//        Board board = boardRepository.findbyId(boardId).orElseThrow(() -> new NullPointerException("registerComment ID를 찾을 수 없습니다."));
+//        CommentValidation.validationCommentRegister(requestDto);
+//
+//        Member member = memberRepository.findById(userDetails.getUser().getId()).orElseThrow(("createComment ID 오류"));
+//        Comment comment = new Comment(requestDto.getContent, board, member);
+//        commentRepository.save(comment);
+//
+//        CreateCommentResponseDto commentResponseDto = new CreateCommentResponseDto();
+//        commentResponseDto.setComment_Id(comment.getId());
+//        commentResponseDto.setCreatedAt(comment.getCreatedAt());
+//
+//        return commentResponseDto;
+//    }
 
     @Transactional
     public void updateComment(Long commentId, CommentRequestDto registerDto) {
@@ -49,6 +66,19 @@ public class CommentService {
         commentRepository.delete(comment);
     }
 
+    public List<CommentResponseDto> getComment(Long boardId) {
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("getComment ID 오류"));
+        List<Comment> commentList = commentRepository.findAllByBoard(board);
 
+        List<CommentResponseDto> commentResponseDtoList = new ArrayList<>();
+        for (Comment comment : commentList) {
+            CommentResponseDto commentResponseDto = new CommentResponseDto();
+            commentResponseDto.setContent(comment.getContent());
+            commentResponseDto.setUsername(comment.getMember().getUsername());
+            commentResponseDtoList.add(commentResponseDto);
+        }
+
+        return commentResponseDtoList;
+    }
 
 }
