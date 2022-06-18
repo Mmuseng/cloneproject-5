8dbc2ff42595328930f10e4feb0ef29e52f44e3e
MinjuKim <kpoong02@naver.com>
2022-06-19 01:00:26 +0900

    Update CommentRepository.java

--- src/main/java/com/hanghae99/cloneproject5/repository/CommentRepository.java
+++ src/main/java/com/hanghae99/cloneproject5/repository/CommentRepository.java
@@ -1,7 +1,12 @@
 package com.hanghae99.cloneproject5.repository;
 
+import com.hanghae99.cloneproject5.model.Board;
 import com.hanghae99.cloneproject5.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findAllByBoard (Board board);
 }
